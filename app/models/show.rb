123d34cd7bc262a7e857d42f0c36dc81d90be864
Bobby Neal <rsneal01@gmail.com>
2020-10-13 17:18:22 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
 
 
   def self.most_popular_show
-    most_popular_show = Show.where(rating: "Show.highest_rating")
+    most_popular_show = Show.find_by rating: "Show.highest_rating"
   end
   
 end
